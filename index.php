fc5f1b4971d893bfa19d3090be49acdcdd626da0
julioUrjc <j.martinsae@alumnos.urjc.es>
2015-07-15 10:29:14 +0200

    index inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<title>Atlas Project</title>
+<FRAMESET ROWS="189,*,40" FRAMESPACING="0" FRAMEBORDER="0" BORDER="0">
+	<FRAME SRC="cabecera.php" NAME="MENU">
+ 	<FRAMESET COLS="*,290" FRAMESPACING="0" FRAMEBORDER="0" BORDER="0">
+		
+		<FRAME SRC="hola.php" NAME="todo">
+		<FRAME SRC="twitter.php" NAME="tweet"> 
+	</FRAMESET>
+		<FRAME SRC="pie.php" NAME="pie">		
+<NOFRAMES>
+<body>
+
+<p><I>ESTE DOCUMENTO REQUIERE UN NAVEGADOR QUE VISUALICE FRAMES.</I></P>
+</body>
+</NOFRAMES>
+</FRAMESET>
+</html>
